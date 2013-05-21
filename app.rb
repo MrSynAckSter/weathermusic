332d0e446e6401f67f6ff82e830facd48d6373a3
John Dunlap <freejazztampa@yahoo.com>
2013-05-21 12:57:39 -0400

    project

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
 require 'sinatra'
 require 'forecast_io'
-
-
  
 get '/' do
 	@wetout = "default"
@@ -16,21 +14,153 @@ post '/' do
 	month = params[:post][:month] 
 	day = params[:post][:day] 
         location = params[:post][:location] 
-	@wetout = "#{year} year! #{month} is the month! #{day} is the day!"  
+	@wetout = "Welcome \n"  
         Weatherparse.forecast(year,month,day,location)
         @pressure = Weatherparse.pressure
-        @wetout = "#{@wetout} and here's some weather #{@pressure} is the pressure \n //BEGIN CODE"  
+	@temperature = Weatherparse.temperature 
+	@precipIntensity = Weatherparse.precipIntensity 
+	@dewpoint = Weatherparse.dewpoint 
+	@humidity = Weatherparse.humidity 
+	@pressure = Weatherparse.pressure 
+	@visibility = Weatherparse.visibility 
+        @wetout = "\n 
+        //BEGIN CODE \n
+	(  
+        Server.default.options.memSize = 512*1024;
+        s.boot; 
+        "
+        if @temperature <= 30 
+        @wetout = "#{@wetout} \n 
+        SynthDef(\030,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25, pitch= 100 |
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, 0.3)) * Klang.ar(`[ [pitch, pitch + 200, pitch + 400],[0.3, 0.3, 0.3],[pi,pi,pi]], 1, 0) ; 
+  sig = BPF.ar(t,rand(1300) + 100) * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \\instrument, \\030,
+                \\pitch, Prand([200,210,300,100,330], inf), 
+                \\dur, Prand([ 2,3,4,5,0.5,0.1,3], inf),
+                \\amp, 0.5).play;" 
+        end 
+        if @temperature >= 30  
+        @wetout = "#{@wetout} \n 
+	{Saw.ar(Line.ar(rand(300) + 300 ,rand(200) + 200 ,0.25),1,1) * EnvGen.ar(Env.perc(0.01,0.3)) }.play;
+
+SynthDef(\\6090,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25, pitch= 100 |
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, 0.3)) * Saw.ar(Line.ar(rand(300) + pitch ,rand(200) + 200 ,0.25),1,1) ; 
+  y = BPF.ar(t,rand(1300) + 100);
+  sig = GVerb.ar(y, 10, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \\instrument, \\6090,
+                \\pitch, Prand([200,210,300,100,330], inf), 
+                \\dur, Prand([ 2,3,4,5,0.5,0.1,3], inf),
+                \\amp, 0.3).play;"        
+	end
+	if @temperature >= 90 
+	@wetout = "#{@wetout} \n
+	{SinOsc.ar(LFNoise1.kr(4,60,80),1,0.25).distort * SinOsc.kr(20,1,100000) * EnvGen.ar(Env.perc(0.01,8))
+}.play;
+
+{SinOsc.ar(LFNoise1.kr(4,1000,2000),1,0.25).distort * SinOsc.kr(20,1,100000)}.play;"
+	end  
+	@wetout = "#{@wetout} \n 
+	{
+HPF.ar(LPF.ar(BPF.ar(WhiteNoise.ar(0.4), 50, 0.4), 500) * Saw.ar(LFNoise2.ar(0.25,7),1,1) * 3 -0.25, 500);
+
+}.play;
+
+{ LPF.ar(BPF.ar( WhiteNoise.ar(0.4), 50, 0.4), 500) * 10 }.play;"	 
+	if @precipIntensity >= 2
+        @wetout = "#{@wetout} \n 
+	var x;
+x = HPF.ar(PinkNoise.ar(0.5).distort * 2, 200) * 0.1;
+
+GVerb.ar(x, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + x;
+
+}.play;"
+	end 
+	if @precipIntensity >= 3
+	@wetout = "#{@wetout} \n 
+	{var t;
+ var y; 
+t = EnvGen.kr(Env.perc(0.01, rand(5)+ 0.5)) * WhiteNoise.ar(0.5); 
+
+
+y = BPF.ar(t,rand(1300) + 100);
+
+GVerb.ar(y, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y;
+}.play" 
+	end
+	if @precipIntensity > 1 
+	@wetout = "#{@wetout} \n
+	
+SynthDef(\\thunder,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25|
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, rand(5)+ 0.5)) * WhiteNoise.ar(0.5); 
+  y = BPF.ar(t,rand(1300) + 100);
+  sig = GVerb.ar(y, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \\instrument, \\thunder,
+                \\dur, Prand([ 2,3,4,10,24,0.5,0.1,34], inf),
+                \\amp, 0.5).play;"
+	end
+	if @dewpoint >= 1
+	@wetout= "#{@wetout} \n {(SinOsc.ar(LFNoise2.ar(#{@dewpoint} / 5 ,800,#{@dewpoint} / 100) + 800,1,#{@dewpoint} / 300))}.play"
+	end	
+	if @humidity >= 1
+	@wetout= "#{@wetout} \n  {DelayN.ar(Decay.ar(Dust.ar(#{@humidity},0.5), 0.3, SinOsc.ar),3,0.5)}.play;"
+	end 
+        if @pressure >= 1 
+        @wetout = "#{@wetout} \n {LPF.ar(SinOsc.ar(SinOsc.kr(#{@pressure} / 1000 ,1, 40 + LFNoise1.ar(1,1,3)) + 5,1,0.25),400) }.play" 
+        end 
+        if @visibility <= 10 
+	@wetout = "#{@wetout} \n {Impulse.ar(LFNoise1.ar(10 - #{@visibility},3,8),1,1)}.play;  
+	{Dust.ar(LFNoise1.ar(5,3,8),1)}.play;  
+	"
+	end 
+        
+        @wetout = "#{@wetout} \n )" 
 	erb :weather
 end
 
 class Weatherparse
-   def self.forecast(year = 2013,month = 1,day = 2,location)  
+   def self.forecast(year = 2013,month = 8,day = 2,location)  
       Forecast::IO.configure do |configuration|
         configuration.api_key = '284bc96ec58d70d124724420603ff493'
       end 
       def self.pressure 
       @@pressure 
       end 
+      def self.temperature 
+      @@temperature 
+      end 
+      def self.precipIntensity 
+      @@precipItensity 
+      end 
+      def self.dewpoint 
+      @@dewpoint 
+      end 
+      def self.humidity 
+      @@humidity 
+      end 
+      def self.pressure 
+      @@pressure 
+      end 
+      def self.visibility 
+      @@visibility 
+      end
       puts location
       if location == "New York" 
       longitude = 40.7142
@@ -56,10 +186,7 @@ class Weatherparse
       puts longitude
       puts latitude 
       end 
-      forecast = Forecast::IO.forecast(longitude,latitude, time: Time.new(year,month = 8 ,day = 1).to_i)
-      forecast.time = Time.new(year = 2013,month = 1,day = 2).to_i
-      #if location == "New York" 
-     # forecast.longitude =  
+      forecast = Forecast::IO.forecast(longitude,latitude, time: Time.new(year,month = 8,day ).to_i ) 
       @@pressure = forecast.currently.pressure  
       puts "#{@@pressure} is the pressure" 
       @@precipItensity = forecast.currently.precipIntensity 
---
+++ weatherprototypes.sc
@@ -0,0 +1,195 @@
+//boilerplate 
+(
+
+Server.default.options.memSize = 512*1024;
+s.boot; 
+)
+
+(
+{
+var x;
+x = HPF.ar(PinkNoise.ar(0.5).distort * 2, 200) * 0.1;
+
+GVerb.ar(x, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + x;
+
+}.play;
+
+
+// Thunder 
+
+{var t;
+ var y; 
+t = EnvGen.kr(Env.perc(0.01, rand(5)+ 0.5)) * WhiteNoise.ar(0.5); 
+
+
+y = BPF.ar(t,rand(1300) + 100);
+
+GVerb.ar(y, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y;
+}.play;
+
+SynthDef(\thunder,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25|
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, rand(5)+ 0.5)) * WhiteNoise.ar(0.5); 
+  y = BPF.ar(t,rand(1300) + 100);
+  sig = GVerb.ar(y, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \instrument, \thunder,
+                \dur, Prand([ 2,3,4,10,24,0.5,0.1,34], inf),
+                \amp, 0.5).play;
+
+
+
+)
+
+
+(
+
+//"temperature":67.77,
+
+// if the temperature is below 30 
+{ Klang.ar(`[ [800, 1000, 1200],[0.3, 0.3, 0.3],[pi,pi,pi]], 1, 0) * EnvGen.ar(Env.perc(0.01,0.3))}.play;
+
+
+SynthDef(\030,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25, pitch= 100 |
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, 0.3)) * Klang.ar(`[ [pitch, pitch + 200, pitch + 400],[0.3, 0.3, 0.3],[pi,pi,pi]], 1, 0) ; 
+  sig = BPF.ar(t,rand(1300) + 100) * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \instrument, \030,
+                \pitch, Prand([200,210,300,100,330], inf), 
+                \dur, Prand([ 2,3,4,5,0.5,0.1,3], inf),
+                \amp, 0.5).play;
+
+
+
+
+// if the temperature is between 30 and 60 
+
+
+
+
+
+
+// if the temperature is between 60-90 
+
+{Saw.ar(Line.ar(rand(300) + 300 ,rand(200) + 200 ,0.25),1,1) * EnvGen.ar(Env.perc(0.01,0.3)) }.play;
+
+SynthDef(\6090,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25, pitch= 100 |
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, 0.3)) * Saw.ar(Line.ar(rand(300) + pitch ,rand(200) + 200 ,0.25),1,1) ; 
+  y = BPF.ar(t,rand(1300) + 100);
+  sig =t + y * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \instrument, \6090,
+                \pitch, Prand([200,210,300,100,330], inf), 
+                \dur, Prand([ 2,3,4,5,0.5,0.1,3], inf),
+                \amp, 0.5).play;
+
+// if the temperature is between 90-100 
+
+/*/{SinOsc.ar(LFNoise1.kr(4,60,80),1,0.25).distort * SinOsc.kr(20,1,100000) * EnvGen.ar(Env.perc(0.01,8))
+}.play;
+
+{SinOsc.ar(LFNoise1.kr(4,1000,2000),1,0.25).distort * SinOsc.kr(20,1,100000)}.play
+/*/
+
+
+// "precipIntensity":0,
+
+
+// Farnell Rain on Ground 
+
+{
+HPF.ar(LPF.ar(BPF.ar(WhiteNoise.ar(0.4), 50, 0.4), 500) * Saw.ar(LFNoise2.ar(0.25,7),1,1) * 3 -0.25, 500);
+
+
+
+}.play;
+
+{ LPF.ar(BPF.ar( WhiteNoise.ar(0.4), 50, 0.4), 500) * 10 }.play;
+
+// General Ambient Rain Noise 
+
+{
+var x;
+x = HPF.ar(PinkNoise.ar(0.5).distort * 2, 200) * 0.1;
+
+GVerb.ar(x, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + x;
+
+}.play;
+
+
+// Thunder 
+
+{var t;
+ var y; 
+t = EnvGen.kr(Env.perc(0.01, rand(5)+ 0.5)) * WhiteNoise.ar(0.5); 
+
+
+y = BPF.ar(t,rand(1300) + 100);
+
+GVerb.ar(y, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y;
+}.play;
+
+
+
+
+SynthDef(\thunder,
+{| freq = 440, gate = 1, sustain = 1, amp= 0.25|
+  var sig, t,y; 
+  t = EnvGen.kr(Env.perc(0.01, rand(5)+ 0.5)) * WhiteNoise.ar(0.5); 
+  y = BPF.ar(t,rand(1300) + 100);
+  sig = GVerb.ar(y, 105, 5, 0.7, 0.8, 60, 0.1, 0.5, 0.4) + y * amp;
+  Out.ar(0, sig ! 2) 
+}).add;
+
+p = Pbind(
+                \instrument, \thunder,
+                \dur, Prand([ 2,3,4,10,24,0.5,0.1,34], inf),
+                \amp, 0.5).play;
+
+
+        
+
+
+
+//"dewPoint":46.65,
+{(SinOsc.ar(LFNoise2.ar(43 / 5 ,800,43 / 200) + 800,1,43 / 300))}.play
+
+//"windSpeed":19.64,
+
+
+
+// "cloudCover":0.12,
+
+
+
+// "humidity":0.46, 
+ {DelayN.ar(Decay.ar(Dust.ar(1,0.5), 0.3, SinOsc.ar),3,0.5)}.play;
+
+
+// "pressure":1020.64, Low pressure contributes to stormyness 
+
+// Low Pressure 
+{LPF.ar(SinOsc.ar(SinOsc.kr(0.125 ,1, 40 + LFNoise1.ar(1,1,3)) + 5,1,0.25),400) }.play;
+
+// "visibility":10, capped at 10. 2<, <2, 10 
+
+
+{Impulse.ar(LFNoise1.ar(5,3,8),1,1)}.play;  
+{Dust.ar(LFNoise1.ar(5,3,8),1)}.play;  
+
+)
+                               
